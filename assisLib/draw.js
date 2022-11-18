0e85cf354680c8099846a8a87472f4750a1a25d1
BruceChen <sharkmansky@163.com>
2022-11-18 12:37:47 +0800

    进一步泛化了draw接口，使其可以支持不同类型的绘制，新增renderPrimitive函数，通过鼠标点击可以绘制指定类型的几何体

--- assisLib/draw.js
+++ assisLib/draw.js
@@ -1,9 +1,16 @@
-function draw(gl,shader,uniformOperation) {
+function draw(gl,shader,drawType,uniformOperation) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    var glTypeMap = {
+        point : gl.POINTS,
+        polyline : gl.LINE_STRIP,
+        polygon : gl.LINE_LOOP,
+        line : gl.LINES,
+        region : gl.TRIANGLE_STRIP,
+    }
 
     var attributeNameArray = Object.keys(shader.vertexAttributeValues);
     for(var i = 0; i < attributeNameArray.length; i++){
@@ -29,10 +36,53 @@ function draw(gl,shader,uniformOperation) {
     if(!uniformOperation) uniformOperation(gl,shader.uniformLocations);
 
     {
+        const glDrawType = glTypeMap[drawType];
+        console.log('!');
         const offset = 0;
         const vertexCount = shader.vertexAttributeValues[attributeNameArray[0]].vertexNum;
-        gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+        gl.drawArrays(glDrawType, offset, vertexCount);
     }
     
     
+}
+
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+
+    var x = ((event.clientX - rect.left) / (rect.width / 2) - 1).toFixed(2);
+    var y = (1 - (event.clientY - rect.top) / (rect.width / 2)).toFixed(2);
+
+    return [x,y];
+}
+
+function initMouseControl(canvas) {    
+    var aPos = [];
+    canvas.addEventListener("click", function(event) {
+        aPos.push(getMousePos(canvas, event));
+        console.log(aPos); 
+    });
+}
+
+
+function renderPrimitive(canvas,primitiveType,color) {
+    var aPos = [];
+    var aColor = [];
+    canvas.addEventListener("click", function(event) {
+        aPos.push(getMousePos(canvas, event));
+        aColor.push(color);
+        const gl = canvas.getContext('webgl');
+        const pointShader = new ShaderInfo(
+            './shaderSrc/vs.glsl',
+            './shaderSrc/fs.glsl',
+            {aPos:aPos,aColor:aColor},
+            {}
+        );
+        initShader(gl,pointShader).then((shader)=>{
+            draw(gl,shader,primitiveType,()=>{});
+        });
+    });
+}
+
+function rgb(r,g,b) {
+    return [r/255,g/255,b/255];
 }
--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-function main() {
-    const gl = document.querySelector('#canvas').getContext('webgl');
-    const testInfo = new ShaderInfo(
-        './shaderSrc/vs.glsl',
-        './shaderSrc/fs.glsl',
-        {
-            aPos : [
-                [ 1.0, 1.0],
-                [-1.0, 1.0],
-                [ 1.0,-1.0],
-                [-1.0,-1.0]
-            ],
-            aColor : [
-                [1.0,1.0,1.0],
-                [1.0,0.0,0.0],
-                [0.0,1.0,0.0],
-                [0.0,0.0,1.0]
-            ],
-        },
-        {
 
-        }
-    ); 
-    initShader(gl,testInfo).then((shader)=>{
-        draw(gl,shader,()=>{});
-    })
+
+
+function main() {
+    const canvas = document.querySelector('#canvas');
+    renderPrimitive(canvas,'polygon',rgb(128,209,200));
 }
